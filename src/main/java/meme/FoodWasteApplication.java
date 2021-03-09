6f0263011bafdd21f426fbfa36774de846d41436
Ines L <inesluparia@gmail.com>
2021-03-09 09:54:51 +0100

    first

---
+++ src/main/java/meme/FoodWasteApplication.java
@@ -0,0 +1,13 @@
+package meme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodWasteApplication.class, args);
+    }
+
+}
